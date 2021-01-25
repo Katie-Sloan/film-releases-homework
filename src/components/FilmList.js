d87398a874f526bf8722c535a7f815d4b36b13f1
Katie Sloan <katie.a.sloan@gmail.com>
2021-01-25 22:17:13 +0000

    slight amends, homework complete

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -7,7 +7,7 @@ const FilmList = ({films}) => {
     const filmArray = films.map(film => {
         return(
             <p className="film-list">
-            <Film name={film.name} key={film.id}>{film.url}</Film>
+                <Film name={film.name} key={film.id}>{film.url}</Film>
             </p>
         )
     })
